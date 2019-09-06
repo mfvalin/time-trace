80c3e9234a098378d10d5a356f2957bbfe020019
M. Valin <mfvalin@gmail.com>
2019-09-06 18:23:55 -0400

    first pass with robodoc documentation

--- time_trace.h
+++ time_trace.h
@@ -12,6 +12,97 @@
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //  Lesser General Public License for more details.
+//
+//****P* libtimetrace/set of timing routines for Fortran and C  
+*/
+// DESCRIPTION
+//  This package contains functions to get the wall clock time, the time of the day, the number of machine cpu_clock_cycles
+//  from an arbitrary time point in the past (some functions may only work on amd64 architecture CPUs).
+//
+//  This package also contains a set of functions to perform a "time trace" of an application.
+//
+//  Post-processing tools will eventually be created to help interpreting these time traces.
+//
+//  In the case of a MPI program, there would normally be one time trace per process, unless the application only calls
+//  the time trace functions on a reduced set of processes.
+//
+// EXAMPLES
+int main(){        // non MPI C example of use
+  time_context t;
+  int i, tag;
+  void *array[100];
+  int larray[100];
+
+  printf("C Test of time trace \n");
+  TimeTraceInit(&t);
+  printf("-------------------------\n");
+  TimeTrace(t, 0);
+  TimeTraceStep(t, 0);
+  printf("=========================\n");
+  TimeTraceBarr(t, 1, 1, 0, NULL);
+  for(i=1 ; i<4 ; i++){
+    TimeTraceStep(t, i);
+    printf("++++ start step %d ++++\n",i);
+    tag = 10*i+1;
+    TimeTraceBarr(t, tag, 1, 0, NULL);
+    printf("++++ end step %d ++++\n",i);
+    tag = 10*i+2;
+    TimeTrace(t, tag);
+  }
+  TimeTraceStep(t, 4);
+  printf("=========================\n");
+   TimeTrace(t, 77777777);
+   TimeTraceDumpText(t, "time_list", 0);
+   TimeTraceDumpBinary(t, "time_list", 0);
+   TimeTraceGetBuffers(t, array, larray, 10);
+   for(i=0 ; i<10 ; i++) printf("%d ",larray[i]);
+   printf("\n");
+  return 0;
+}
+
+program test_trace      ! MPI example of use in Fortran
+  use ISO_C_BINDING
+  implicit none
+#include <time_trace.hf>
+  include 'mpif.h'
+  integer :: ierr
+  integer :: i, tag, rank
+  type(time_context) :: t
+  type(C_PTR), dimension(10) :: array
+  integer(C_INT), dimension(10) :: larray
+  external :: MPI_barrier
+
+  rank = 0
+  call MPI_init(ierr)
+  call MPI_comm_rank(MPI_COMM_WORLD, rank, ierr)
+  call time_trace_init(t)
+  print *,'-----------------------------'
+  call time_trace(t, 0)
+  call time_trace_step(t, 0)
+  print *,'============================='
+  call time_trace_barr(t, 1, .true., MPI_COMM_WORLD, MPI_barrier)
+  do i = 1, 3
+    call time_trace_step(t, i)
+    print *,'+++++++ start step =',i
+    tag = 10*i+1
+    call time_trace_barr(t, tag, .true., MPI_COMM_WORLD, MPI_barrier)
+    print *,'+++++++ end   step =',i
+    tag = 10*i+2
+    call time_trace(t, tag)
+  enddo
+  call time_trace_step(t, 4)
+  print *,'============================='
+  call time_trace(t, 999999)
+  call time_trace_dump_text(t, 'time_list', rank)
+  call time_trace_dump_binary(t, 'time_list', rank)
+  call time_trace_get_buffers(t, array, larray, 10)
+  write(6,'(10I6)')larray
+  call MPI_finalize(ierr)
+
+  stop
+end program
+//**** 
+*/
 #endif
 
 #if defined SELF_TEST
@@ -21,19 +112,29 @@
 #endif
 
 #if defined(FORTRAN_SOURCE)
-type, BIND(C) :: time_context  ! wrappper to give a type to a C pointer
-  type(C_PTR) :: t
+!****T* libtimetrace/user defined types  (Fortran version)
+! SYNOPSIS
+!  use #include <time_trace.hf>
+
+type, BIND(C) :: time_context   !  wrappper to give a type to a C pointer
+  type(C_PTR) :: t              ! actual pointer to internal trace table
 end type
 
-type, bind(C) :: timeval
-  integer(C_LONG_LONG) :: sec
-  integer(C_LONG_LONG) :: usec
+type, bind(C) :: timeval        !  interface to structure returned by get_time_of_day
+  integer(C_LONG_LONG) :: sec   !  seconds for an arbitrary time in the past
+  integer(C_LONG_LONG) :: usec  !  microseconds (added to sec)
 end type
-
+!****
 #else
-typedef struct{
-  void *t;
+//****T* libtimetrace/user defined types  (C version) 
+/**/
+// SYNOPSIS
+// use #include <time_trace.h>
+
+typedef struct{      //  struct wrapper to give a type to a C pointer to a trace table
+  void *t;           // actual pointer to trace table
 }time_context;
+//****   */
 #endif
 
 #if ! defined(FORTRAN_SOURCE)
@@ -62,38 +163,80 @@ typedef struct{
 #if defined(FORTRAN_SOURCE)
 interface
 #if ! defined(NEED_PRIVATE)
+!****f* libtimetrace/time_trace_dump_text  (Fortran version)
+! Synopsis
+!  Dump the accumulated time trace into a text file, using a free format
+!  the file name will be filename_nnnnnn.txt (nnnnnn from ordinal)
+!  filename amy be a relative path (some_name) or an absolute path (/some/path/to/some/name)
+!
+!  t        : opaque time context object (from time_trace_init)
+!  filename : file name prefix (will be trimmed to remove trailing blanks if any)
+!  ordinal  : numbered extension to file name (nnnnnn) (normally the MPI rank)
+!
+! ARGUMENTS
   subroutine time_trace_dump_text(t, filename, ordinal)   ! dump timings int file filename_nnnnnn.txt (nnnnnn from ordinal)
     import :: time_context
     implicit none
-    type(time_context), intent(IN) :: t              ! opaque time context pointer (from time_trace_init)
+    type(time_context), intent(IN) :: t              ! opaque time context object (from time_trace_init)
     character(len=*), intent(IN) :: filename         ! file name prefix (will be trimmed to remove trailing blanks if any)
     integer, intent(IN) :: ordinal                   ! numbered extension to file name (nnnnnn) (normally MPI rank)
+!****
   end subroutine time_trace_dump_text
 
+!****f* libtimetrace/time_trace_dump_binary  (Fortran version)
+! Synopsis
+!  Dump the accumulated time trace into a C binary file (NATIVE ENDIANNESS)
+!  the file name will be filename_nnnnnn.dat (nnnnnn from ordinal)
+!  filename amy be a relative path (some_name) or an absolute path (/some/path/to/some/name)
+!
+!  t        : opaque time context object (from time_trace_init)
+!  filename : file name prefix (will be trimmed to remove trailing blanks if any)
+!  ordinal  : numbered extension to file name (nnnnnn) (normally the MPI rank)
+!
+! ARGUMENTS
   subroutine time_trace_dump_binary(t, filename, ordinal)   ! dump timings int file filename_nnnnnn.dat (nnnnnn from ordinal)
     import :: time_context
     implicit none
-    type(time_context), intent(IN) :: t              ! opaque time context pointer
+    type(time_context), intent(IN) :: t              ! opaque time context object
     character(len=*), intent(IN) :: filename         ! file name prefix (will be trimmed to remove trailing blanks if any)
     integer, intent(IN) :: ordinal                   ! numbered extension to file name (nnnnnn) (normally MPI rank)
+!****
   end subroutine time_trace_dump_binary
 
+!****f* libtimetrace/time_trace_barr  (Fortran version)
+! Synopsis
+!  Add a "barriered" pair of time traces. The first time will be taken before the "barrier" call, the second
+!  time will be taken after the "barrier". This allows to measure the "load imbalance" across the barrier.
+!  This function is normally intended to be used by an MPI application.
+!  ( "barriered" version of function time_trace)
+!
+!  t        : opaque time context object (from time_trace_init)
+!  tag      : tag number associated with this timing point (MUST be >0 and <99999999)
+!  barrier  : if .true., the function issues a barrier call and get two times
+!             if .false. the function behaves like function time_trace
+!  comm     : integer value (normally a MPI communicator) passed to barrier_code
+!  barrier_code : "barrier" function to be called between the two times
+!             call barrier_code(comm, ierr)  (ierr is an integer variable)
+!             MPI_barrier would be the usual value passed to time_trace_barr
+!
+! ARGUMENTS
   subroutine time_trace_barr(t, tag, barrier, comm, barrier_code)  ! insert a new time trace entry (2 entries if barrier is true)
     import :: time_context
-    type(time_context), intent(IN) :: t              ! opaque time context pointer (from time_trace_init)
+    type(time_context), intent(IN) :: t              ! opaque time context object (from time_trace_init)
     integer, intent(IN) :: tag                       ! tag number for this timing point (MUST be >0 and <99999999)
     logical, intent(IN) :: barrier                   ! if true, call MPI_barrier with timing points before and after
     integer, intent(IN) :: comm                      ! MPI communicator (only used if barrier flag is true)
     external :: barrier_code
+!****
   end subroutine time_trace_barr
 #endif
 
 #if defined(NEED_PRIVATE)
   subroutine new_time_tag(t, tag, t1, t2) bind(C,name='TimeTraceNewTag') ! insert tag and 1 or 2 time deltas
     import :: time_context, C_INT, C_LONG_LONG
-    type(time_context), intent(IN), value :: t              ! opaque time context pointer
-    integer(C_INT), intent(IN),value :: tag
-    integer(C_LONG_LONG), intent(IN), value :: t1, t2
+    type(time_context), intent(IN), value :: t              ! opaque time context object
+    integer(C_INT), intent(IN),value :: tag                 ! 32 bit integer
+    integer(C_LONG_LONG), intent(IN), value :: t1, t2       ! 64 bit integers
   end subroutine new_time_tag
 
   function what_time_is_it() result (t) bind(C,name='WallClockTime')
@@ -102,41 +245,108 @@ interface
   end function what_time_is_it
 #endif
 
+!****f* libtimetrace/wall_clock_time  (Fortran version)
+! Synopsis
+!  Returns elapsed wall clock time since an arbitrary time in the past (time is in microseconds)
+! ARGUMENTS
+  function wall_clock_time() result (t) bind(C,name='WallClockTime')
+    import :: C_LONG_LONG
+    INTEGER(C_LONG_LONG) t     ! 64 bit integer
+!****
+  end function wall_clock_time
+
+!****f* libtimetrace/cpu_clock_cycles  (Fortran version)
+! Synopsis
+!  Returns the number of CPU clock cycles since an arbitrary time in the past (usually last boot time)
+!  The CPU is assumed to be running at its "nominal frequency" (insensitive to the actual clock speed that may vary)
+! ARGUMENTS
+  function cpu_clock_cycles() result (t) bind(C,name='CpuClockCycles')
+    import :: C_LONG_LONG
+    INTEGER(C_LONG_LONG) t     ! 64 bit integer
+!****
+  end function cpu_clock_cycles
+
+!****f* libtimetrace/get_time_of_day  (Fortran version)
+! Synopsis
+!  Interface to the C library function gettimeofday (see user defined type timeval above)
+!  Returns the elapsed time from an arbitrary time in the past
+!
+!  output argument tv is type(timeval) (see above)
+!  argument tz should be C_NULL_PTR as no Fortran user defined type is available for the Time Zone
+! ARGUMENTS
   function get_time_of_day(tv,tz) result(time) BIND(C,name='gettimeofday') ! interface to C library function gettimeofday
     import :: timeval, C_PTR, C_INT
     implicit none
     type(timeval), intent(OUT)     :: tv
-    type(C_PTR), value, intent(IN) :: tz
+    type(C_PTR), value, intent(IN) :: tz    ! for the time being this should be C_NULL_PTR
     integer(C_INT) :: time
+!****
   end function get_time_of_day
 
+!****f* libtimetrace/time_trace_init  (Fortran version)
+! Synopsis
+!  Create a "time context", used to accumulate a time trace
+!  (multiple threads could have independent time traces if necessary)
+!
+!  t        : opaque time context object (to be used in subsequent calls to the package functions)
+! ARGUMENTS
   subroutine time_trace_init(t) bind(C,name='TimeTraceInit')! create and initialize a new time trace context
     import :: time_context
     implicit none
-    type(time_context), intent(OUT) :: t                    ! opaque time context pointer (passed to other routines)
+    type(time_context), intent(OUT) :: t                    ! opaque time context object (passed to other routines)
+!****
   end subroutine time_trace_init
 
+!****f* libtimetrace/time_trace_step  (Fortran version)
+! Synopsis
+!  Start of a new "time step" (or timing epoch). Until the next "time step" times will be stored
+!  relative to the start of "time step" ( up o ~ 2000 seconds before overflow)
+!
+!  t        : opaque time context object (from time_trace_init)
+!  step     : time step number (arbitrary, user defined, MUST be >0 and <99999999)
+!
+! ARGUMENTS
   subroutine time_trace_step(t, step) bind(C,name='TimeTraceStep')  ! set step value for subsequent calls to time_trace
     import :: time_context, C_INT
     implicit none
-    type(time_context), intent(IN), value :: t              ! opaque time context pointer (from time_trace_init)
+    type(time_context), intent(IN), value :: t              ! opaque time context object (from time_trace_init)
     integer(C_INT), intent(IN), value :: step               ! time step number (MUST be >0 and <99999999)
+!****
   end subroutine time_trace_step
 
+!****f* libtimetrace/time_trace  (Fortran version)
+! Synopsis
+!  Record a timing point with an associated tag
+!
+!  t        : opaque time context object (from time_trace_init)
+!  tag      : arbitrary number identifying this timing point (MUST be >0 and <99999999)
+! ARGUMENTS
   subroutine time_trace(t, tag)  bind(C,name='TimeTrace') ! insert a new time trace entry (no barrier)
     import :: time_context, C_INT
     implicit none
-    type(time_context), intent(IN), value :: t              ! opaque time context pointer (from time_trace_init)
+    type(time_context), intent(IN), value :: t              ! opaque time context object (from time_trace_init)
     integer(C_INT), intent(IN), value :: tag                ! tag number for this timing point (MUST be >0 and <99999999)
+!****
   end subroutine time_trace
 
+!****f* libtimetrace/time_trace_get_buffers  (Fortran version)
+! Synopsis
+!  Get timing buffers and their associated lengths. If there are less than n buffers available, the
+!  unused entries in array will be filled with C_NULL_PTR and the unused entries in larray will be filled with 0
+!
+!  t        : opaque time context object (from time_trace_init)
+!  array    : C pointer array that will receive the C addresses of the buffers
+!  larray   : integer array that 
+!  n        : dimension of array and larray
+! ARGUMENTS
   subroutine time_trace_get_buffers(t, array, larray, n) bind(C,name='TimeTraceGetBuffers')
     import :: C_PTR , C_INT, time_context
     implicit none
-    type(time_context), intent(IN), value :: t              ! opaque time context pointer (passed to other routines)
+    type(time_context), intent(IN), value :: t              ! opaque time context object (from time_trace_init)
     type(C_PTR), dimension(n), intent(OUT) :: array         ! to receive pointers to buffers
-    integer(C_INT), dimension(n), intent(OUT) :: larray     ! to receive lengths of buffers
-    integer(C_INT), intent(IN), value :: n                  ! size of array and larray 
+    integer(C_INT), dimension(n), intent(OUT) :: larray     ! to receive lengths of buffers (32 bit integers)
+    integer(C_INT), intent(IN), value :: n                  ! size of array and larray (32 bit integer)
+!****
   end subroutine time_trace_get_buffers
 
 end interface
@@ -148,13 +358,85 @@ end interface
   void TimeTraceNewTag(time_context t, int tag, long long t1, long long t2);
   void TimeTraceNewStep(time_context t, int step);
 #endif
+//****f* libtimetrace/WallClockTime (C version)
+// */
+// Synopsis
+//  Returns elapsed wall clock time since an arbitrary time in the past (time is in microseconds)
+// ARGUMENTS
+  uint64_t WallClockTime();
+//**** */
+//****f* libtimetrace/CpuClockCycles (C version)
+//*/
+// Synopsis
+//  Returns the number of CPU clock cycles since an arbitrary time in the past (usually last boot time)
+//  The CPU is assumed to be running at its "nominal frequency" (insensitive to the actual clock speed that may vary)
+// ARGUMENTS
+  uint64_t CpuClockCycles(void);
+//**** */
+//****f* libtimetrace/TimeTraceGetBuffers (C version)
+// */
+// Synopsis
+//  See time_trace_get_buffers
+// ARGUMENTS
   void TimeTraceGetBuffers(time_context t, void **array, int *larray, int n);
+//**** */
+//****f* libtimetrace/TimeTraceInit (C version)
+// */
+// Synopsis
+//  See time_trace_init
+// ARGUMENTS
   void TimeTraceInit(time_context *t);
+//**** */
+//****f* libtimetrace/TimeTraceStep (C version)
+// */
+// Synopsis
+//  See time_trace_step
+// ARGUMENTS
   void TimeTraceStep(time_context t, int step);
+//**** */
+//****f* libtimetrace/TimeTrace (C version)
+// */
+// Synopsis
+//  See time_trace
+// ARGUMENTS
   void TimeTrace(time_context t, int tag);
+//**** */
+//****f* libtimetrace/TimeTraceBarr (C version)
+// */
+// Synopsis
+//   Add a "barriered" pair of time traces. The first time will be taken before the "barrier" call, the second
+//   time will be taken after the "barrier". This allows to measure the "load imbalance" across the barrier.
+//   This function is normally intended to be used by an MPI application.
+//   ( "barriered" version of function TimeTrace)
+//
+//   DEFERRED FULL IMPLEMENTATION (as of now, barrier_code and comm are IGNORED)
+//   two timing entries are created, to remain consistent with the Fortran version
+// 
+//   t        : opaque time context object (from time_trace_init)
+//   tag      : tag number associated with this timing point (MUST be >0 and <99999999)
+//   barrier  : if non zero, the function issues a barrier call and get two times
+//              if equal to zero. the function behaves like function TimeTrace
+//   comm     : integer value (normally a Fortran MPI communicator) passed to barrier_code
+//   barrier_code : "barrier" function to be called between the two times
+//              ierr = barrier_code(comm)  (ierr is an integer variable)
+//              MPI_barrier would be the usual value passed to time_trace_barr
+// ARGUMENTS
   void TimeTraceBarr(time_context t, int tag, int barrier, int fcomm, void (*barrier_code)());
-  void TimeTraceDump(time_context t, char *filename, int ordinal);
+//**** */
+//****f* libtimetrace/TimeTraceDumpText (C version)
+// */
+// Synopsis
+//  See time_trace_dump_text
+// ARGUMENTS
+  void TimeTraceDumpText(time_context t, char *filename, int ordinal);
+//**** */
+//****f* libtimetrace/TimeTraceDumpBinary (C version)
+// */
+// Synopsis
+//  See time_trace_dump_binary
+// ARGUMENTS
   void TimeTraceDumpBinary(time_context t, char *filename, int ordinal);
+//**** */
 #endif
 
 #if defined(FORTRAN_SOURCE)
--- time_trace_c.c
+++ time_trace_c.c
@@ -13,6 +13,7 @@
 //  Lesser General Public License for more details.
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -23,7 +24,7 @@
 #include <time_trace.h>
 #undef NEED_PRIVATE
 
-long long WallClockTime(){           // get current time of day in microseconds
+uint64_t WallClockTime(){           // get current time of day in microseconds
   struct timeval tv;
   long long time;
   gettimeofday(&tv, NULL);
@@ -32,6 +33,27 @@ long long WallClockTime(){           // get current time of day in microseconds
   time += tv.tv_usec;               // add microseconds part
   return time;
 }
+
+static uint64_t current_cpu_clock = 0;
+
+uint64_t CpuClockCycles(void) {   // version with serialization
+#if defined(__x86_64__)
+  uint32_t lo, hi;
+  __asm__ volatile ("rdtscp"
+      : /* outputs   */ "=a" (lo), "=d" (hi)
+      : /* no inputs */
+      : /* clobbers  */ "%rcx");
+  return current_cpu_clock = (uint64_t)lo | (((uint64_t)hi) << 32) ;
+#else
+#if defined(__aarch64__)
+  asm volatile ("isb; mrs %0, cntvct_el0" : "=r" (current_cpu_clock));
+  return current_cpu_clock;
+#else
+  return current_cpu_clock++;
+#endif
+#endif
+}
+
 
 // tp : pointer to a time context structure
 void TimeTraceInit(time_context *tp){  // create initialized time context
